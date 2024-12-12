387b54f5cd3088a165380fb16cf79689cea51653
nekocatchan <f@oken.fun>
2024-12-12 12:19:32 +0900

    🎨 kv を使用するところで kv.js から import する

--- src/db/db.js
+++
@@ -1,8 +0,0 @@
-import KeyFactory from "/db/utils/key_factory.js";
-
-const kv = await Deno.openKv();
-
-// sample
-export async function getTicket(ticketId) {
-  return (await kv.get(KeyFactory.ticketKey(ticketId))).value;
-}
